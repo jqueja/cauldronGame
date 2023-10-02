371fdee8841e3b21afdd8ce6d11c8ac22c8de3f5
Josh <jqueja@calpoly.edu>
2023-10-02 11:33:40 -0700

    fixed the catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -28,12 +28,28 @@ def get_catalog():
         amountReturn = redPotions
     '''
 
-    return [
-            {
-                "sku": "RED_POTION",
-                "name": "red potion",
-                "quantity": getRedPotions(),
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
-        ]
+    numRedPotions =  getRedPotions()
+
+    if numRedPotions == 0:
+        lstReturn = []
+
+        return [
+                {
+                    "sku": "RED_POTION",
+                    "name": "red potion",
+                    "quantity": lstReturn,
+                    "price": 50,
+                    "potion_type": [100, 0, 0, 0],
+                }
+            ]
+    else:
+        return [
+                {
+                    "sku": "RED_POTION",
+                    "name": "red potion",
+                    "quantity": numRedPotions,
+                    "price": 50,
+                    "potion_type": [100, 0, 0, 0],
+                }
+            ]
+
